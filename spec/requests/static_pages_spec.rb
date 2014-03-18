ad37c5f9db5f325c2994fe42832cdb7adb5bcf9e
Tubagus Ghandy <tb.wagon@yahoo.com>
2014-03-18 15:40:54 +0700

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,18 +41,24 @@ describe "Static pages" do
     it { should have_title(full_title('Contact')) }
   end
   
+  describe "Sign up page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+  
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
     expect(page).to have_title(full_title('About Us'))
     click_link "Help"
-    expect(page).to # fill in
+    expect(page).to have_title(full_title('Help'))
     click_link "Contact"
-    expect(page).to # fill in
+    expect(page).to have_title(full_title('Contact'))
     click_link "Home"
     click_link "Sign up now!"
-    expect(page).to # fill in
+    expect(page).to have_title(full_title('Sign up'))
     click_link "sample app"
-    expect(page).to # fill in
   end
 end
