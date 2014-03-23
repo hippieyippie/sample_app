a41e9540fe5fb0318a137b7fcb4d427c667dbe6d
Tubagus Ghandy <tb.wagon@yahoo.com>
2014-03-23 23:46:47 +0700

    Finish sign in

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+# Assets should be precompiled for production (so we don't need the gems loaded then)
+Bundler.require( *Rails.groups( assets: %w(development test) ))
 
 module SampleApp
   class Application < Rails::Application
@@ -25,7 +25,7 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-	  # config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-	  config.assets.initialize_on_precompile = false
+	  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+	  # config.assets.initialize_on_precompile = false
   end
 end
